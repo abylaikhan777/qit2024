6ed5619151f59b678b51a5c45fb58a93ad9a2bff
tyrimbet abylaikhan <abeke.618@gmail.com>
2024-10-31 18:00:46 +0500

    first commit

---
+++ skript.js
@@ -0,0 +1,109 @@
+// var name = " a;soief"; //string
+// var age = 17; //number
+
+// var isNight =true;
+
+// var students = ['sd', 'du', 'sdf']
+
+// console.log(typeof name);
+// console.log(typeof name)
+// console.log(typeof name)
+//  var na = "yera";
+
+//  var me = "syl"
+
+//  console.log(na + me);
+
+
+//  var na = "5";
+
+//  var me = "9"
+
+//  console.log(na - me);
+
+//  var name = "Abulaikhan";
+//  var surname = "Turimbet";
+//  var birthday = "01.90.2011";
+//  var otan = "Kazahstan";
+
+//  console.log("Menin atym" , name)
+//  console.log("Menin surnam" , surname)
+//  console.log("Menin tugankunim" , birthday)
+//  console.log("Menin otanym" , otan)
+
+//  var a = 45;
+//  var b = 25;
+
+//  var total = a * b;
+
+//  console.log(total);
+
+// var num = 20;
+// let num2 = 2;
+
+// console.log(num / num2); 
+
+// var num1 = 45;
+// var num3 = 3;
+
+// console.log(num1 % num3)
+
+// var messages = 200;
+// var money = 20;
+
+// money++;
+
+// console.log(messages * money)
+// var a = -200;
+
+// a = a + 3;
+
+// a++;
+// a--;
+
+// console.log(a); //-197
+
+
+// var nam = 150;
+
+// nam += 30;
+
+// console.log(nam)
+
+// let x = 5;
+
+// x -= 2; //x = x - 2
+
+// console.log(x)
+
+// var bar = 300;
+
+// bar *= 2;
+// console.log(bar)
+
+// let y = 400;
+
+
+// let q = 3;
+
+// y /= q;
+// console.log(y);
+
+// var a = 10;
+
+// a %= 2;
+
+// console.log(a) 
+
+
+
+
+var a = 32;
+var b = 20;
+var c = 10;
+var d = 15;
+var e = 70;
+
+let total = (a + b) * c - d / e; 
+
+console.log(total)
